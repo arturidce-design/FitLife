fc50ea62dbca3b7e08c13cf84b147e1f32ce0a96
Artur Ivanov <arturidce@gmail.com>
2026-04-21 19:16:16 +0500

    исправлены ошибки с отступом, добавлен метод try/except, исправлен метод capitalize на title, объявлена константа для красивого ответа

--- fit_life.py
+++ fit_life.py
@@ -5,6 +5,7 @@ sys.stdout.reconfigure(encoding="utf-8")
 
 WATER_PER_KG = 30
 LITERS_IN_ML = 1000
+OUTPUT_SEPARATOR = "\n" + "=" * 40 + "\n"
 
 
 def calculate_bmi(weight, height):
@@ -21,21 +22,34 @@ def calculate_water_norm(weight):
 print("Добро пожаловать в калькулятор здоровья FitLife!")
 
 #  Получение данных от пользователя.
-user_name = input("Введите ваше имя: ").capitalize()
-user_age = int(input(f"Здравствуйте, {user_name}! Введите ваш возраст: "))
-user_weight = float(input("Введите ваш вес в килограммах: "))
-user_height = float(input("Введите ваш рост в метрах: "))
+user_name = input("Введите ваше имя: ").title()
+try:
+    user_age = int(input(f"Здравствуйте, {user_name}! Введите Ваш возраст: "))
+except ValueError:
+    user_age = int(input("Ошибка. Введите корректное число для возраста: "))
+try:
+    user_weight = float(input("Введите ваш вес в килограммах: "))
+except ValueError:
+    user_weight = float(input("Ошибка. Введите корректное число для веса: "))
+try:
+    user_height = float(input("Введите ваш рост в метрах: "))
+except ValueError:
+    user_height = float(input("Ошибка. Введите корректное число для роста: "))
 
 bmi = calculate_bmi(user_weight, user_height)
 water_norm = calculate_water_norm(user_weight)
 
 #  Вывод результатов.
-print(f"\n{'=' * 40}\n")
-print(f"Спасибо, {user_name}! Вот ваши результаты:")
-print("Ваш Индекс Массы Тела (BMI): "
-      f"{bmi}")
-print("Рекомендуемое количество воды в день: "
-      f"{water_norm} литров")
-print(f"\n{'=' * 40}\n")
-print("Благодарим за использование калькулятора здоровья FitLife! "
-      "Берегите себя и будьте здоровы!")
+print(OUTPUT_SEPARATOR)
+print(f"Спасибо, {user_name}! Вот Ваши результаты:")
+print(
+    f"Ваш Индекс Массы Тела (BMI): {bmi}"
+)
+print(
+    f"Рекомендуемое количество воды в день: {water_norm} литров"
+)
+print(OUTPUT_SEPARATOR)
+print(
+    "Благодарим за использование калькулятора здоровья FitLife!\n"
+    "Берегите себя и будьте здоровы!"
+)
